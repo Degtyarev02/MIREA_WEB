d7e670a5ad550df1724a954117aa530e55defa9f
Vladimir <vladimirdegty@gmail.com>
2021-11-19 14:33:48 +0300

    to-do list

--- all practices/practice9/clock/script.js
+++ all practices/practice9/clock/script.js
@@ -5,9 +5,24 @@ function showTime() {
     let minutes = document.getElementById("minutes");
     let seconds = document.getElementById("seconds");
 
-    hours.textContent = date.getHours();
-    minutes.textContent = date.getMinutes();
-    seconds.textContent = date.getSeconds();
+
+    if (date.getHours() < 10) {
+        hours.textContent = '0' + date.getHours();
+    } else {
+        hours.textContent = date.getHours();
+    }
+
+    if (date.getMinutes() < 10) {
+        minutes.textContent = '0' + date.getMinutes();
+    } else {
+        minutes.textContent = date.getMinutes();
+    }
+
+    if (date.getSeconds() < 10) {
+        seconds.textContent = '0' + date.getSeconds();
+    } else {
+        seconds.textContent = date.getSeconds();
+    }
 
     setTimeout(showTime, 1000);
 }
---
+++ all practices/practice9/to-do list/script.js
@@ -0,0 +1,47 @@
+let enterButton = document.getElementById("enter");
+let input = document.getElementById("userInput");
+let ulList = document.querySelector("ul");
+let item = document.getElementsByTagName("li");
+
+function inputLength(){
+    return input.value.length;
+}
+
+
+
+function createListElement() {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ulList.appendChild(li);
+    input.value = "";
+
+
+    let dBtn = document.createElement("button");
+    dBtn.appendChild(document.createTextNode("-"));
+    li.appendChild(dBtn);
+
+
+    function addToDone() {
+        li.classList.toggle("done");
+    }
+
+    li.addEventListener("click",addToDone);
+    
+    function deleteListItem(){
+        li.classList.add("delete")
+    }
+
+    dBtn.addEventListener("click", deleteListItem);
+}
+
+
+function addListAfterClick(){
+    if (inputLength() > 0) {
+        createListElement();
+    }
+}
+
+
+
+enterButton.addEventListener("click",addListAfterClick);
+
