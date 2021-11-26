ba6d19d9d00a363156d79fdd97e4e3e06fb57be1
Vladimir <vladimirdegty@gmail.com>
2021-11-26 14:43:28 +0300

    practice10

---
+++ all practices/practice10/task2/script.js
@@ -0,0 +1,20 @@
+function countDown() {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    const countDown = new Date("12/20/2021").getTime(),
+        x = setInterval(function () {
+
+            const now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+        }, 0)
+}
+
+countDown();
